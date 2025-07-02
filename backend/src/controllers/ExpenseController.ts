77d4384790e3ed8d8e5340e74e6d608aea7ded18
Vinicius Aguiar <viniciusaguiar52@hotmail.com>
2025-07-02 00:37:49 -0300

    feat: Created Controller

---
+++ backend/src/controllers/ExpenseController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+import { ExpenseService } from '../services/ExpenseService';
+
+const expenseService = new ExpenseService();
+
+class ExpenseController {
+    async getAllExpenses(req: Request, res: Response) {
+        try {
+            const expenses = await expenseService.getAllExpenses();
+            res.status(200).json({ data: expenses, success: true });
+        } catch (error: any) {
+            res.status(500).json({ error: error.message, success: false });
+        }
+    }
+
+    async getExpenseById(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            const expense = await expenseService.getExpenseById(id);
+            res.status(200).json({ data: expense, success: true });
+        } catch (error: any) {
+            res.status(500).json({ error: error.message, success: false });
+        }
+    }
+
+    async createExpense(req: Request, res: Response) {
+        try {
+            const expense = await expenseService.createExpense(req.body);
+            res.status(200).json({ data: expense, success: true });
+        } catch (error: any) {
+            res.status(500).json({ error: error.message, success: false });
+        }
+    }
+
+    async updateExpense(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            const expense = await expenseService.updateExpense(id, req.body);
+            res.status(200).json({ data: expense, success: true });
+        } catch (error: any) {
+            res.status(500).json({ error: error.message, success: false });
+        }
+    }
+
+    async deleteExpense(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            const expense = await expenseService.deleteExpense(id);
+            res.status(200).json({ data: expense, success: true });
+        } catch (error: any) {
+            res.status(500).json({ error: error.message, success: false });
+        }
+    }
+}
+
+export { ExpenseController };
+
