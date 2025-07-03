69ce2be2402e807109df36e59ca2931bbf83e722
Vinicius Aguiar <viniciusaguiar52@hotmail.com>
2025-07-03 01:41:24 -0300

    feat: Updated app.ts

--- backend/src/config/app.ts
+++ backend/src/config/app.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import expenseRoutes from '../routes/expense.routes';
+import categoryRoutes from '../routes/category.routes';
+import paymentTypeRoutes from '../routes/paymentType.routes';
 
 dotenv.config();
 
@@ -13,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/despesas', expenseRoutes);
+app.use('/api/categorias', categoryRoutes);
+app.use('/api/tipos-de-pagamento', paymentTypeRoutes);
 
 app.get('/', (req, res) => {
   res.send('API Orçamento Doméstico funcionando!');
